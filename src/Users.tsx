c1889b62997ee64454a3ab8f680bd68da8e0a80d
Phil Heslop <109079089+philipheslop@users.noreply.github.com>
2024-05-30 19:22:02 +0000

    added use\context to select and display current user

--- src/Users.tsx
+++ src/Users.tsx
@@ -1,11 +1,18 @@
 import { useQuery } from "react-query";
+import { createContext, useState } from "react";
 import { getUsers, User } from "./getUsers";
 import { UsersTable } from "./UsersTable";
 
+export const CurrentUserContext = createContext(null);
+
 export const Users = () => {
   const { isLoading, data } = useQuery<User[]>(["users"], getUsers);
+  const [currentUser, setCurrentUser] = useState(null);
+
   console.log(isLoading);
   console.log(data);
+  console.log(currentUser);
+
   return (
     <>
       <div className="App">
@@ -13,7 +20,19 @@ export const Users = () => {
           <div>Loading...</div>
         ) : (
           <>
-            <UsersTable />
+            {currentUser === null ? (
+              <div>No User Selected</div>
+            ) : (
+              <div>
+                <h2>Current User</h2>
+                Name: {currentUser.name}
+                Email: {currentUser.email}
+                Status: {currentUser.status}
+              </div>
+            )}
+            <CurrentUserContext.Provider value={setCurrentUser}>
+              <UsersTable />
+            </CurrentUserContext.Provider>
           </>
         )}
       </div>
--- src/UsersTableBody.tsx
+++ src/UsersTableBody.tsx
@@ -1,4 +1,9 @@
+import { useContext } from "react";
+import { CurrentUserContext } from "./Users";
+
 export const UsersTableBody = ({ coloumns, processedData }) => {
+  const setCurrentUser = useContext(CurrentUserContext);
+
   return (
     <tbody>
       {processedData.map((user) => {
@@ -6,9 +11,10 @@ export const UsersTableBody = ({ coloumns, processedData }) => {
           <tr
             key={user.id}
             className="border-b border-neutral-200 dark:border-white/10"
+            onClick={() => setCurrentUser(user)}
           >
             {coloumns.map(({ coloumnId }) => {
-              const tData = user[coloumnId] ? user[coloumnId] : "——";
+              const tData = user[coloumnId] ? user[coloumnId] : "n/a";
               return <td key={coloumnId}>{tData}</td>;
             })}
           </tr>
